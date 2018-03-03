be7a3931f0b8f998c614365e5696890af4d5e9e4
Siddharth Vadgama <me@siddv.net>
2018-03-03 16:48:51 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # splitscreen
+
+> Simple chrome extension to display webpages next to each other, when opening a new tab.
+
+
+Based on the [webpack simple](https://github.com/vuejs-templates/webpack-simple) Vue template 
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install 
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+## Development
+
+Load this folder as an unpacked extension on [chrome://extensions](chrome://extensions)
+
+Edit `./src/App.vue` and rerun `npm run dev`
---
+++ src/App.vue
@@ -0,0 +1,167 @@
+<template>
+  <div id="app">
+
+    <div class="site-wrap">
+      <input
+        class="site-entry"
+        type="text"
+        placeholder="Enter website here and press 'enter'"
+        @keyup.enter="updateSites"
+        v-model="userInput.left">
+
+      <iframe 
+        class="site"
+        v-if="sites.left"
+        :src="sites.left">
+      </iframe>
+    </div>
+
+    <div class="site-wrap">
+      <input
+        class="site-entry"
+        type="text"
+        placeholder="Enter website here and press 'enter'"
+        @keyup.enter="updateSites"
+        v-model="userInput.right">
+
+      <iframe
+        class="site"
+        v-if="sites.right"
+        :src="sites.right">
+      </iframe>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      userInput: {
+        left: 'sdfs',
+        right: 'sdfsd'
+      },
+      sites: {
+        left: 'sdfs',
+        right: 'sdfsd'
+      }
+    }
+  },
+  mounted () {
+    
+    chrome.storage.sync.get('sites', (data) => {
+
+      this.sites.left = data.sites.left;
+      this.userInput.left = data.sites.left;
+
+      this.sites.right = data.sites.right;
+      this.userInput.right = data.sites.right;
+
+      this.addListeners();
+    })
+
+  },
+  methods: {
+    /**
+     * Nullifies x-frame-options headers to allow iframe injection
+     * @param details details of the HTTP request
+     */
+    onHeadersReceived (details) {
+
+      if(details.initiator != window.location.origin) {
+        return;
+      }
+
+      for (let index in details.responseHeaders) {
+
+        let header = details.responseHeaders[index];
+
+        if (header.name.toLowerCase() == 'x-frame-options' ||
+            header.name.toLowerCase() == 'frame-ancestors') {
+
+          details.responseHeaders.splice(index, 1);
+          return {
+            responseHeaders: details.responseHeaders
+          };
+        }
+      }
+    },
+
+    /**
+     * Adds the header listeners when the sites update
+     */
+    addListeners () {
+
+      for (let site in this.sites) {
+        if(this.sites[site]) {
+          chrome.webRequest.onHeadersReceived.addListener(
+            this.onHeadersReceived,
+            { urls: [this.sites[site]] },
+            [ "blocking", "responseHeaders" ]
+          );
+        }
+      }
+    },
+
+    /**
+     * Update the sites when changed by the user
+     */
+    updateSites (event) {
+
+      event.preventDefault();
+
+      for (let site in this.sites) {
+        if(this.sites[site]) {
+          chrome.webRequest.onHeadersReceived.removeListener(
+            this.onHeadersReceived,
+            { urls: [this.sites[site]] },
+            [ "blocking", "responseHeaders" ]
+          );
+        }
+      }
+
+      this.addListeners();
+
+      chrome.storage.sync.set({
+        sites: {
+          left: this.userInput.left,
+          right: this.userInput.right
+        },
+      }, (data) => {
+  
+        this.sites.left = this.userInput.left;
+        this.sites.right = this.userInput.right;
+  
+      });
+
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+}
+
+.site-wrap {
+  display: flex;
+  flex-direction: column;
+  flex-basis: 50%;
+}
+
+.site-entry {
+  font-size: 16px;
+  padding: 8px;
+}
+
+.site {
+  border: 0;
+  flex-grow: 1;
+}
+</style>
